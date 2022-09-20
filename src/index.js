4f86d6791df35a0168911ac00e155e2f151d3db6
May Wu <maywu4@gmail.com>
2022-09-20 14:53:58 -0400

    Add submit button and event listener to update graph upon submit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { config } from './scripts/graph'
 import Chart from 'chart.js/auto';
 
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
     const lineChart = new Chart(
         document.getElementById('lineChart'),
         config
     );
-})
 
+// })
+
+export default lineChart;
--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,28 +1,59 @@
+import { Chart } from 'chart.js';
 import * as Calculations from './calculations.js';
+import lineChart from '../index'
 
+//source: https://www.chartjs.org/docs
+let userInput = 0  
 
-//source: https://www.chartjs.org/docs/latest/getting-started/
-let userInput = 0  //updateUserInput();
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const input = document.getElementById('userInput');
-    console.log(input)
-    
-    const updateUserInput = function(ev) {
-        ev.preventDefault();
-        userInput = document.getElementById('userInput').value
-        console.log(userInput);
-        return false;
-    }
-    input.addEventListener("submit", function(e) {
+// document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.getElementById('input');
+
+    form.addEventListener("submit", (e) => {
         e.preventDefault();
-        userInput = document.getElementById('userInput').value
-        console.log(userInput);
+        let amount = document.getElementById('inputAmount').value
+        userInput = parseInt(amount);
+        lineChart.data.datasets = [
+            {
+                label: 'Inflation',
+                backgroundColor: '#b4cd93',
+                borderColor: '#b4cd93',
+                data: [Math.floor(userInput), Calculations.calcValueDueToInflation(userInput, 1), Calculations.calcValueDueToInflation(userInput, 3), Calculations.calcValueDueToInflation(userInput, 5), Calculations.calcValueDueToInflation(userInput, 10), Calculations.calcValueDueToInflation(userInput, 20)],
+            },
+            {
+                label: 'Savings Account',
+                backgroundColor: '#a7d7c5',
+                borderColor: '#a7d7c5',
+                data: [Math.floor(userInput), Calculations.calcValueInSavings(userInput, 1), Calculations.calcValueInSavings(userInput, 3), Calculations.calcValueInSavings(userInput, 5), Calculations.calcValueInSavings(userInput, 10), Calculations.calcValueInSavings(userInput, 20)],
+            },
+            {
+                label: 'CDs(Certificates of Deposit)',
+                backgroundColor: '#74b49b',
+                borderColor: '#74b49b',
+                data: [Math.floor(userInput), Calculations.calcValueCd(userInput, 1), Calculations.calcValueCd(userInput, 3), Calculations.calcValueCd(userInput, 5), Calculations.calcValueCd(userInput, 10), Calculations.calcValueCd(userInput, 20)],
+            },
+            {
+                label: 'U.S. Treasury Bonds',
+                backgroundColor: '#5c8d89',
+                borderColor: '#5c8d89',
+                data: [Math.floor(userInput), Calculations.calcValueTreasuryBond(userInput, 1), Calculations.calcValueTreasuryBond(userInput, 3), Calculations.calcValueTreasuryBond(userInput, 5), Calculations.calcValueTreasuryBond(userInput, 10), Calculations.calcValueTreasuryBond(userInput, 20)],
+            },
+            {
+                label: 'S&P 500 ETFs',
+                backgroundColor: '#427a5b',
+                borderColor: '#427a5b',
+                data: [Math.floor(userInput), Calculations.calcValueSP500(userInput, 1), Calculations.calcValueSP500(userInput, 3), Calculations.calcValueSP500(userInput, 5), Calculations.calcValueSP500(userInput, 10), Calculations.calcValueSP500(userInput, 20)],
+            },
+            {
+                label: 'Stocks(GOOGL, KO, MFST, APPL, META)',
+                backgroundColor: '#118C4F',
+                borderColor: '#118C4F',
+                data: [Math.floor(userInput), Calculations.calcValueStocks(userInput, 1), Calculations.calcValueStocks(userInput, 3), Calculations.calcValueStocks(userInput, 5), Calculations.calcValueStocks(userInput, 10), Calculations.calcValueStocks(userInput, 20)],
+            }
+        ]
+        lineChart.update();
     })
-})
-
-
-
+// });
 
 
 const labels = [
